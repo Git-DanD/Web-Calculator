75540124a098b1bea8258ded4754c6b964d206db
DanD <bren.dandxyz@gmail.com>
2024-06-26 10:09:57 +1000

    Added functionality to operation buttons and updated display

--- script.js
+++ script.js
@@ -6,30 +6,64 @@ class Calc {
     }
 
     clear() {
-        this.currenOperand = ''
+        this.currentOperand = ''
         this.previousOperand = ''
         this.operation = undefined
     }
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNumber(number) {
-        if (number === '.' && this.currenOperand.includes('.')) return
-        this.currenOperand = this.currenOperand.toString() + number.toString()
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
     }
 
     chooseOperation(operation) {
-
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
     }
 
     compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
 
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            case '*':
+                computation = prev * current
+                break
+            default:
+                return
+        }
+
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
     }
 
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currenOperand
+        this.currentOperandTextElement.innerText = this.currentOperand
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = 
+            `${this.previousOperand} ${this.operation}`
+        }
     }
 }
 
@@ -48,4 +82,26 @@ numberButtons.forEach(button => {
         calc.appendNumber(button.innerText)
         calc.updateDisplay()
     })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calc.chooseOperation(button.innerText)
+        calc.updateDisplay()
+    })
+})
+
+equalsButtons.addEventListener('click', button => {
+    calc.compute()
+    calc.updateDisplay()
+})
+
+allClearButtons.addEventListener('click', button => {
+    calc.clear()
+    calc.updateDisplay()
+})
+
+deleteButtons.addEventListener('click', button => {
+    calc.delete()
+    calc.updateDisplay()
 })
